71b2d1b0b798deb46bb93fe72fa5bf7e44b87933
GraceSchwar <grace.schwar@gmail.com>
2020-10-05 19:04:24 -0400

    basic html/css/js set up with starting content

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+//when they select a spot, push x/o to that array position
+
+
+const gameBoard = (() => {
+    const gameboard = ['X1', 'X2', 'X3', 'X4', 'O5', 'O6', 'O7', 'O8', 'X9'];
+    return {gameboard}
+})();
+
+const createPlayer = (name, symbol) => {
+    return {name, symbol}
+}
+
+const displayController = (() =>{
+
+})();
+
+/* 3. Set up your HTML and write a JavaScript function that will render the contents of the gameboard array 
+to the webpage (for now you can just manually fill in the array with "X"s and "O"s)
+*/
+
+//shows moves from gameboard[] on webpage
+function renderMoves() {
+
+}
