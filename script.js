30f15d12e6825f3811ed72ef7118139d49092a45
GraceSchwar <grace.schwar@gmail.com>
2020-10-09 15:51:57 -0400

    renderMoves() links array item to each square

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 
 const gameBoard = (() => {
-    const gameboard = ['X1', 'X2', 'X3', 'X4', 'O5', 'O6', 'O7', 'O8', 'X9'];
+    const gameboard = ['X0', 'X1', 'X2', 'X3', 'X4', 'X5', 'X6', 'X7', 'X'];
     return {gameboard}
 })();
 
@@ -20,7 +20,55 @@ const displayController = (() =>{
 to the webpage (for now you can just manually fill in the array with "X"s and "O"s)
 */
 
-//shows moves from gameboard[] on webpage
+//shows moves from gameboard[] on webpage and fills box with X marker in array
 function renderMoves() {
 
+    const board = gameBoard.gameboard
+
+    for (let i=0; i<board.length; i++){
+        console.log(board[i]);
+        console.log(board.indexOf(i));
+        switch (board[i]) {
+            case board[0]:
+                const box0 = document.getElementById('box0');
+                box0.textContent = 'TEST-0';
+                break;
+            case board[1]: 
+                const box1 = document.getElementById('box1');
+                box1.textContent = 'TEST-1';
+                break;
+            case board[2]: 
+                const box2 = document.getElementById('box2');
+                box2.textContent = 'TEST-2';
+                break;
+            case board[3]:
+                const box3 = document.getElementById('box3');
+                box3.textContent = 'TEST-3';
+                break;
+            case board[4]: 
+                const box4 = document.getElementById('box4');
+                box4.textContent = 'TEST-4';
+                break;
+            case board[5]: 
+                const box5 = document.getElementById('box5');
+                box5.textContent = 'TEST-5';
+                break;
+            case board[6]:
+                    const box6 = document.getElementById('box6');
+                    box6.textContent = 'TEST-6';
+                    break;
+            case board[7]: 
+                    const box7 = document.getElementById('box7');
+                    box7.textContent = 'TEST-7';
+                    break;
+            case board[8]: 
+                const box8 = document.getElementById('box8');
+                box8.textContent = 'TEST-8';
+                break;
+        //cases for each of the 9 array items to add content of them to the corrosponding squares in dom.
+        }
+    }
+    console.log('rendered');
 }
+
+renderMoves();
