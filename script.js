baa9e04ff8109dccd509b53c9a0481ba11e7be99
GraceSchwar <grace.schwar@gmail.com>
2020-10-13 19:16:15 -0400

    checkWinner() works

--- script.js
+++ script.js
@@ -4,17 +4,24 @@ const gameBoard = (() => {
     return {board}
 })();
 
-//shows moves from gameboard[] in their corrosponding box in the browser
+//Factory Function: Creates player and allows them to make moves
+const player = (name, symbol) => {
+    //name = form.value
+    return {name,symbol}
+}
+
+const player1 = player('player1', 'X');
+const player2 = player('player2', 'O');
+
+
+//Loop through every item  in board[] and push array value to corrosponding dom square
 function renderMoves() {
     const board = gameBoard.board;
 
-    //Loop through every item  in board[] and push array value to corrosponding dom square
     for (let i=0; i<board.length; i++){
-        console.log(board[i]);
-        const targetBox = document.getElementById(`${i}`);     console.log(targetBox);
+        const targetBox = document.getElementById(`${i}`);
         targetBox.textContent = board[i]; 
     }
-    console.log('rendered');
 }
 
 //Module: when box is clicked, pushes players marker to the array index corrosponding to the box, and renders page to show marker in box. 
@@ -23,9 +30,20 @@ const playGame = (() =>{
     const board = gameBoard.board;
     let symbol = ''; 
     
+    function checkWinner() {
+        if (board[0] === board[1] && board[1] === board[2]) {console.log("YOU WIN")}
+        if (board[3] === board[4] && board[4] === board[5]) {console.log("YOU WIN")}
+        if (board[6] === board[7] && board[7] === board[8]) {console.log("YOU WIN")}
+        if (board[0] === board[3] && board[3] === board[6]) {console.log("YOU WIN")}
+        if (board[1] === board[4] && board[4] === board[7]) {console.log("YOU WIN")}
+        if (board[2] === board[5] && board[5] === board[8]) {console.log("YOU WIN")}
+        if (board[0] === board[4] && board[4] === board[8]) {console.log("YOU WIN")}
+        if (board[2] === board[4] && board[4] === board[6]) {console.log("YOU WIN")}
+    }
+
     //when spot it clicked push player's marker to the corrospsong box position in the array 
     const markSpot = (e) => {
-        const targetArrayIndex = board[`${e.target.id}`] ; console.log(targetArrayIndex); //sets the board square to the corrosponding array index
+        const targetArrayIndex = board[`${e.target.id}`] ; //sets the board square to the corrosponding array index
 
         if (symbol === '') {
             symbol = player1.symbol;
@@ -38,24 +56,11 @@ const playGame = (() =>{
             if (targetArrayIndex === '') {board.splice(`${e.target.id}`,1, symbol)};
         }
         renderMoves();
+        checkWinner(); console.log("check winner")
     }
     spots.forEach((box) => box.addEventListener('click', markSpot))
     return {markSpot}  
 })();
 
-//Factory Function: Creates player and allows them to make moves
-const player = (name, symbol) => {
-    const {markSpot} = playGame; //gives players the ability to mark spots from playGame()
-    return {name,symbol, markSpot}
-}
 
-const player1 = player('player1', 'X');
-const player2 = player('player2', 'O');
-
-
-//check winner
-
-function checkWinner() {
- 
-}
 
