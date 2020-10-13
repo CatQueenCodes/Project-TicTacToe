1f3481fb8d5192f4c3305eee369676deaf909499
GraceSchwar <grace.schwar@gmail.com>
2020-10-13 16:28:47 -0400

    logic for markSpot()

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 const gameBoard = (() => {
-    const board = ['', '', '', 'X3', 'X4', 'X5', 'X6', 'X7', 'X8'];
+    const board = ['', '', '', '', '', '', '', '', ''];
     return {board}
 })();
 
 //shows moves from gameboard[] in their corrosponding box in the browser
 function renderMoves() {
-
     const board = gameBoard.board;
 
     //Loop through every item  in board[] and push array value to corrosponding dom square
     for (let i=0; i<board.length; i++){
         console.log(board[i]);
-       const targetBox = document.getElementById(`${i}`);     console.log(targetBox);
-       targetBox.textContent = board[i]; 
+        const targetBox = document.getElementById(`${i}`);     console.log(targetBox);
+        targetBox.textContent = board[i]; 
     }
     console.log('rendered');
 }
 
-//when box is clicked, pushes players marker to the array index corrosponding to the box, and renders page to show marker in box. 
-    //if that array position already contains a marker, don't let them play there
+//Module: when box is clicked, pushes players marker to the array index corrosponding to the box, and renders page to show marker in box. 
 const playGame = (() =>{
     const spots = Array.from(document.getElementsByClassName('box'));
     const board = gameBoard.board;
@@ -27,15 +25,30 @@ const playGame = (() =>{
     const markSpot = (e) => {
         const targetArrayIndex = board[`${e.target.id}`] ; console.log(targetArrayIndex); //sets the board square to the corrosponding array index
 
-        if (targetArrayIndex === '') {board.splice(`${e.target.id}`,1, `${player.symbol}`)}; console.log(board); //sub SYMBOL for play Symbol
+        let symbol = '';
+        let lastSymbolUsed = '';
+
+        if (symbol === '') {
+            symbol = 'X';
+            lastSymbolUsed = 'X';
+            if (targetArrayIndex === '') {board.splice(`${e.target.id}`,1, symbol)}; console.log(symbol, lastSymbolUsed);
+        }else if (lastSymbolUsed === 'X') {
+            symbol = 'O';
+            lastSymbolUsed = 'O';
+            if (targetArrayIndex === '') {board.splice(`${e.target.id}`,1, symbol)}; console.log(symbol, lastSymbolUsed);
+        }else if (lastSymbolUsed === 'O') {
+            symbol = 'X';
+            lastSymbolUsed = 'X';
+            if (targetArrayIndex === '') {board.splice(`${e.target.id}`,1, symbol)}; console.log(symbol, lastSymbolUsed);
+        }
         renderMoves();
     }
     spots.forEach((box) => box.addEventListener('click', markSpot))
-    
+
     return {markSpot}  
 })();
 
-//Creates player and allows them to make moves
+//Factory Function: Creates player and allows them to make moves
 const player = (name, symbol) => {
     const {markSpot} = playGame; //gives players the ability to mark spots from playGame()
     return {name, symbol, markSpot}
